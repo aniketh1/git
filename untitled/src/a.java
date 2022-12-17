a1a90359e812a9eb21a12195f5fc31a4d079bdac
Aniket Korwar <aniketkorwa@gmail.com>
2022-12-17 21:06:26 +0530

    hello this is testing of push command

---
+++ untitled/src/a.java
@@ -0,0 +1,6 @@
+public class a{
+    public static void main(String args[])
+    {
+        System.out.println("Hello i am aniket and i am doing an example of how to push the file in git hub");
+    }
+}
